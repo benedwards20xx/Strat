00d1ae137c9c0d58fd8bd3f628dd65c0c1edf1f4
Benjamin Edwards <edwards_benjamin@bah.com>
2012-10-29 18:27:17 -0400

    Update strat.js, broken atm

--- strat.js
+++ strat.js
@@ -13,9 +13,10 @@ var maxUnitsPerBase = 50;
 
 //object for each player base, should be in order of players, 
 //ie. first in array is player 1, second is player 2, etc.
-var playerBases = [];
+var players = [];
 //base scores (or units, whatever)
 var baseUnits = [];
+
 
 function base(units, maxUnits, x, y, color) {
     this.units = units;
@@ -40,6 +41,21 @@ function base(units, maxUnits, x, y, color) {
     }*/
 }
 
+function player(numBases, color) {
+    this.numBases = numBases;
+    this.color = color;
+    this.bases = [numBases];
+    for (int i = 0; i < numBases; i++) {
+        var randX = Math.floor(Math.random() * (W - EDGE_RESTRICT * 2));
+        var randY = Math.floor(Math.random() * (H - EDGE_RESTRICT * 2));
+        var baseX = randX + EDGE_RESTRICT;
+        var baseY = randY + EDGE_RESTRICT;
+        this.bases = new base(startUnits, maxUnitsPerBase, baseX, baseY, this.color);
+    }
+    this.score = 0;
+}
+
+
 function drawCircle(x, y, inColor, outColor) {
     context.fillStyle = inColor;
     context.lineWidth = '5';
@@ -53,16 +69,16 @@ function drawCircle(x, y, inColor, outColor) {
 
 function setupPlayerBases() {
     for (var i = 0; i < numPlayers; i++) {
-        playerBases[i] = new Array(numBasePerPlayer);
+        players[i] = new player(numBasePerPlayer, playerColors[i]);
     }
 
     for (var i = 0; i < numPlayers; i++) {
-        for (var j = 0; j < playerBases[i].length; j++) {
+        for (var j = 0; j < players[i].bases.length; j++) {
             var randX = Math.floor(Math.random() * (W - EDGE_RESTRICT * 2));
             var randY = Math.floor(Math.random() * (H - EDGE_RESTRICT * 2));
             var baseX = randX + EDGE_RESTRICT;
             var baseY = randY + EDGE_RESTRICT;
-            playerBases[i][j] = new base(startUnits, maxUnitsPerBase, baseX, baseY, playerColors[i]);
+            //players[i].bases[j] = new base(startUnits, maxUnitsPerBase, baseX, baseY, playerColors[i]);
             var tBase = playerBases[i][j];
             drawCircle(tBase.x, tBase.y, tBase.color, tBase.color);
             
@@ -75,7 +91,7 @@ function setupPlayerBases() {
 function drawBases() {
     for (var i = 0; i < numPlayers; i++) {
         for (var j = 0; j < playerBases[i].length; j++) {
-            var tBase = playerBases[i][j];
+            var tBase = players[i].bases[j];
             drawCircle(tBase.x, tBase.y, tBase.color, tBase.color);
             
             context.font = '12pt Helvetica';
@@ -99,7 +115,7 @@ function game() {
 function gameLoop() {
     for (var i = 0; i < numPlayers; i++) {
         for (var j = 0; j < playerBases[i].length; j++) {
-            var tBase = playerBases[i][j];
+            var tBase = players[i].bases[j];
             if (tBase.units < tBase.maxUnits) {
                 tBase.units++;
             } 
@@ -108,6 +124,18 @@ function gameLoop() {
     context.clearRect(0, 0, W, H);
     drawBases();
 }
+
+canvas.addEventListener('mousedown', function (e) {
+    // react to mouse down
+    for (var i = 0; i < numPlayers; i++) {
+        for (var j = 0; j < playerBases[i].length; j++) {
+            var tBase = players[i].bases[j];
+            if (tBase.units >= 10) {
+                tBase.units -= 5;
+            }
+        }
+    }
+});
 
 game();
 loop = setInterval(gameLoop, 1000);
