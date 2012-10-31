216270e6348b1fd820a542c06eb1163654fc73af
Ben Edwards <benguin2kxx@gmail.com>
2012-10-30 20:55:38 -0400

    Added mouse controls and workingness.
    The prototype works mostly, it's definitely messy but I got what I wanted
    to get out of it so check it out. You can add up to four players easily
    and have them "attack" each other.

--- strat.js
+++ strat.js
@@ -2,11 +2,19 @@ var canvas = document.getElementsByTagName( 'canvas' )[ 0 ];
 var context = canvas.getContext( '2d' );
 var loop;
 
-var W = 1200, H = 600;
+var W = canvas.width, H = canvas.height;
 var EDGE_RESTRICT = 60;
 
-var numPlayers = 1;
-var numBasePerPlayer = 1;
+//for mouse drag functionality
+var drag = false;
+//var mouseX, mouseY;
+var clickX, clickY;
+//mouseX = W / 2;
+//mouseY = H / 2;
+var mouseBase;
+var mousePlayerNum;
+
+
 var startUnits = 10;
 var playerColors = [ "blue", "red", 'green', 'orange' ];
 var maxUnitsPerBase = 50;
@@ -14,8 +22,7 @@ var maxUnitsPerBase = 50;
 //object for each player base, should be in order of players, 
 //ie. first in array is player 1, second is player 2, etc.
 var players = [];
-//base scores (or units, whatever)
-var baseUnits = [];
+var baseRadius = 20;
 
 function base(units, maxUnits, x, y, color) {
     this.units = units;
@@ -23,42 +30,22 @@ function base(units, maxUnits, x, y, color) {
     this.x = x;
     this.y = y;
     this.color = color;
-
-    /*function setPosition(x, y) {
-        this.x = x;
-        this.y = y;
-
-    }
-    function draw() {
-        context.strokeStyle = this.color;
-        context.fillStyle = this.color;
-        context.beginPath();
-        context.arc(this.x, this.y, 30, 0, Math.PI * 2);
-        context.closePath();
-        context.stroke();
-        context.fill();
-    }*/
 }
 
-function player(numBases, color) {
+function player(numBases, color, unitRate, score) {
     this.numBases = numBases;
     this.color = color;
     this.bases = [numBases];
+    this.unitRate = unitRate;
     this.score = 0;
     for (var i = 0; i < numBases; i++) {
         var randX = Math.floor(Math.random() * (W - EDGE_RESTRICT * 2));
         var randY = Math.floor(Math.random() * (H - EDGE_RESTRICT * 2));
         var baseX = randX + EDGE_RESTRICT;
         var baseY = randY + EDGE_RESTRICT;
-        this.bases[i] = new base(startUnits, maxUnitsPerBase, baseX, baseY, this.color);
+        this.bases[i] = new base(startUnits * unitRate, maxUnitsPerBase * unitRate, baseX, baseY, this.color);
     }
     //setupBases(this.bases);
-}
-
-function setupBases(bases) {
-    //for each (bases) {
-    //    this.bases = new base(10, 10, 10, 10,this.color);
-    //}
 }
 
 function drawCircle(x, y, inColor, outColor) {
@@ -66,7 +53,7 @@ function drawCircle(x, y, inColor, outColor) {
     context.lineWidth = '5';
     context.strokeStyle = 'black';
     context.beginPath();
-    context.arc(x, y, 30, 0, Math.PI * 2);
+    context.arc(x, y, baseRadius, 0, Math.PI * 2);
     context.closePath();
     context.stroke();
     context.fill();
@@ -75,39 +62,34 @@ function drawCircle(x, y, inColor, outColor) {
 function setupPlayerBases() {
     console.log('setupPlayerBases');
 
-    for (var i = 0; i < numPlayers; i++) {
-        players[i] = new player(numBasePerPlayer, playerColors[i]);
-    }
-
-    for (var i = 0; i < numPlayers; i++) {
+    for (var i = 0; i < players.length; i++) {
         for (var j = 0; j < players[i].bases.length; j++) {
-            var randX = Math.floor(Math.random() * (W - EDGE_RESTRICT * 2));
-            var randY = Math.floor(Math.random() * (H - EDGE_RESTRICT * 2));
-            var baseX = randX + EDGE_RESTRICT;
-            var baseY = randY + EDGE_RESTRICT;
-            //players[i].bases[j] = new base(startUnits, maxUnitsPerBase, baseX, baseY, playerColors[i]);
             var tBase = players[i].bases[j];
             drawCircle(tBase.x, tBase.y, tBase.color, tBase.color);
             
             context.font = '12pt Helvetica';
-            context.fillText(tBase.units, tBase.x - 10, tBase.y + 50);
+            context.fillText(tBase.units, tBase.x - 7, tBase.y + 40);
         }
     }
 }
 
 function drawBases() {
-    for (var i = 0; i < numPlayers; i++) {
-        console.log(players[i].bases.length);
-        for (var j = 0; j < players[i].length; j++) {
+    console.log('drawBases');
+    for (var i = 0; i < players.length; i++) {
+        for (var j = 0; j < players[i].bases.length; j++) {
             var tBase = players[i].bases[j];
-            console.log('tbasex: ' + tBase.x);
             drawCircle(tBase.x, tBase.y, tBase.color, tBase.color);
             
             context.font = '12pt Helvetica';
             context.fillStyle = tBase.color;
-            context.fillText(tBase.units, tBase.x - 10, tBase.y + 50);
+            context.fillText(tBase.units, tBase.x - 7, tBase.y + 40);
         }
     }
+}
+
+function exampleLevel() {
+    players[0] = new player(2, playerColors[0], 1, 0);
+    players[1] = new player(2, playerColors[1], 0.5, 0);
 }
 
 function game() {
@@ -116,14 +98,16 @@ function game() {
 
     context.save();
 
+    exampleLevel();
+
     setupPlayerBases();
 
     context.restore();
 }
 
 function gameLoop() {
-    for (var i = 0; i < numPlayers; i++) {
-        for (var j = 0; j < players[i].length; j++) {
+    for (var i = 0; i < players.length; i++) {
+        for (var j = 0; j < players[i].bases.length; j++) {
             var tBase = players[i].bases[j];
             if (tBase.units < tBase.maxUnits) {
                 tBase.units++;
@@ -134,16 +118,70 @@ function gameLoop() {
     drawBases();
 }
 
+function windowToCanvas(canvas, x, y) {
+    var box = canvas.getBoundingClientRect();
+    return { x: x - box.left * (canvas.width / box.width), 
+             y: y - box.top  * (canvas.height / box.height)
+           };
+}
+
 canvas.addEventListener('mousedown', function (e) {
     // react to mouse down
-    for (var i = 0; i < numPlayers; i++) {
-        for (var j = 0; j < players[i].length; j++) {
-            var tBase = players[i].bases[j];
-            if (tBase.units >= 10) {
-                tBase.units -= 5;
+    for (var i = 0; i < players.length; i++) {
+        if (!drag) {
+            for (var j = 0; j < players[i].bases.length; j++) {
+                var tBase = players[i].bases[j];
+                var dx, dy, dist;
+                dx = e.pageX - this.offsetLeft - tBase.x;
+                dy = e.pageY - this.offsetTop  - tBase.y;
+                dist = Math.sqrt(dx * dx + dy * dy);
+                if (dist < baseRadius) {
+                    drag = true;
+                    mouseBase = tBase;
+                    mousePlayerNum = i;
+                    clickX = dx;
+                    clickY = dy;
+                    break;
+                } 
+                else {
+                    drag = false;
+                }
             }
         }
     }
+});
+
+canvas.addEventListener('mouseup', function (e) {
+    // react to mouse up    
+    //var loc = windowToCanvas(canvas, e.clientX, e.clientY);
+    if (drag) {
+        for (var i = 0; i < players.length; i++) {
+            for (var j = 0; j < players[i].bases.length; j++) {
+                var tBase = players[i].bases[j];
+                var dx, dy, dist;
+                dx = e.pageX - this.offsetLeft - tBase.x;
+                dy = e.pageY - this.offsetTop  - tBase.y;
+                dist = Math.sqrt(dx * dx + dy * dy);
+                if (dist < baseRadius && tBase != mouseBase && i != mousePlayerNum) {
+                    var numUnitsSent = mouseBase.units / 2;
+                    mouseBase.units -= Math.round(numUnitsSent);
+                    tBase.units -= Math.round(numUnitsSent);
+                } else {
+                    var numUnitsSent = 2;
+                    tBase.units -= Math.round(numUnitsSent);
+                }
+            }
+        }
+    }
+    drag = false;
+});
+
+canvas.addEventListener('mousemove', function (event) {
+    /*if(drag) {
+        x = event.pageX - this.offsetLeft - clickX;
+        y = event.pageY - this.offsetTop - clickY;
+        draw();
+    }*/
 });
 
 game();
